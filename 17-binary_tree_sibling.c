3b227e804327f1ee807856410316036ef32e1d68
berekettena <tena21bereket@gmail.com>
2023-03-02 11:48:34 +0300

    Write a function that finds the sibling of a node
    Prototype: binary_tree_t *binary_tree_sibling(binary_tree_t *node);
    Where node is a pointer to the node to find the sibling
    Your function must return a pointer to the sibling node
    If node is NULL or the parent is NULL, return NULL
    If node has no sibling, return NULL

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,7 +11,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	{
 		return (NULL);
 	}
-	if(node->parent->left == node)
+	if (node->parent->left == node)
 	{
 		return (node->parent->right);
 	}
